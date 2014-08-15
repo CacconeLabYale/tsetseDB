135b5cc89388f937d838ec37c7c64a190eb9bdfa
Gus Dunn <wadunn83@gmail.com>
2014-08-15 13:25:31 -0400

    Changes to be committed: 	modified:   .idea/workspace.xml 	modified:   tsetseDB/database_description.py

--- tsetseDB/database_description.py
+++ tsetseDB/database_description.py
@@ -22,8 +22,67 @@ from sqlalchemy import types
 
 base = declarative_base()
 
+
 class Fly(base):
+    """
+    Table class to represent data associated with individual flies:
+    - sex (M|F)
+    - species (f|p|m)
+    - hunger_stage (1|2|3)
+    - wing_fray (int)
+    - stored_in (str)
+    - box_id (ForeignKey?)
+    - infected (bool)
+    - tryps_by_scope (bool)
+    - tryps_by_pcr (bool)
+    - collection_date (date)
+    - trap_id (int, ForeignKey?)
+    - gps_coords (link to TrapTable)
+    - tissues (ask aiden)
+    - teneral (bool)
+    - comments (str)
+
+    """
     __tablename__ = 'fly'
     id = Column("fly_id", types.Integer, primary_key=True)
+    created_on = Column("created_on", types.DateTime)
+    modified_on = Column("modified_on", types.DateTime)
+
+
+class Village(base):
+    """
+    Table class to represent data associated with
+    """
+    __tablename__ = 'village'
+    id = Column("village_id", types.Integer, primary_key=True)
+    created_on = Column("created_on", types.DateTime)
+    modified_on = Column("modified_on", types.DateTime)
+
+
+class Site(base):
+    """
+    Table class to represent data associated with
+    """
+    __tablename__ = 'site'
+    id = Column("site_id", types.Integer, primary_key=True)
+    created_on = Column("created_on", types.DateTime)
+    modified_on = Column("modified_on", types.DateTime)
+
+
+class Tube(base):
+    """
+    Table class to represent data associated with
+    """
+    __tablename__ = 'tube'
+    id = Column("tube_id", types.Integer, primary_key=True)
+    created_on = Column("created_on", types.DateTime)
+    modified_on = Column("modified_on", types.DateTime)
+
+class Box(base):
+    """
+
+    """
+    __tablename__ = 'box'
+    id = Column("box_id", types.Integer, primary_key=True)
     created_on = Column("created_on", types.DateTime)
     modified_on = Column("modified_on", types.DateTime)
