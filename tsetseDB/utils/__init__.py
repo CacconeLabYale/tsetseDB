cdb491ab7bae3e723bd2605efb08de2360cc55e4
Gus Dunn <wadunn83@gmail.com>
2014-08-19 17:52:32 -0400

    created directories: tsetseDB/utils/ and tsetseDB/scripts/

---
+++ tsetseDB/utils/__init__.py
@@ -0,0 +1,14 @@
+# <file_name>.py is part of the '<pagage_name>' package.
+# It was written by Gus Dunn and was created on 00/00/00.
+#
+# Please see the license info in the root folder of this package.
+
+"""
+=================================================
+<file_name>.py
+=================================================
+Purpose:
+
+"""
+
+__author__ = 'Gus Dunn'
