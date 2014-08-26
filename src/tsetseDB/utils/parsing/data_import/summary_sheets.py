31424da2497d74afd0e356159a707821c368dff8
Augustine Dunn <wadunn83@gmail.com>
2014-08-26 15:49:11 -0400

    tsetseDB/utils/parsing/data_import/summary_sheets.py: - added class SummarySheet(object)

--- src/tsetseDB/utils/parsing/data_import/summary_sheets.py
+++ src/tsetseDB/utils/parsing/data_import/summary_sheets.py
@@ -18,142 +18,142 @@ from tsetseDB.utils.parsing import column_data as cdata
 # TODO: Convert below functions into a single class that logs file names etc for useful error reporting
 
 
-def validate_summary_data_format(worksheet):
-    """
-    Returns `True` if `worksheet` passes format tests, `False` otherwise.
-    :param worksheet:
-    :return: `bool`
-    """
+class SummarySheet(object):
 
+    def __init__(self, worksheet, workbook_path):
+        """
+        Returns instance of `SummarySheet` constructed from `worksheet`.
+        :param worksheet: worksheet object
+        :param workbook_path: location of source workbook file
+        """
 
-def get_district(worksheet_row):
-    """
-    Extracts district name from summary sheet row.
+        self.name = worksheet.name
+        self.worksheet = worksheet
+        self.path = workbook_path
 
-    :param worksheet_row: 
-    :return:
-    """
-    return worksheet_row[0]
+    def _validate_summary_data_format(self):
+        """
+        Returns `True` if `worksheet` passes format tests, `False` otherwise.
+        :return: `bool`
+        """
+        raise NotImplementedError()
 
+    def _get_district(self, worksheet_row):
+        """
+        Extracts district name from summary sheet row.
 
-def get_county(worksheet_row):
-    """
-    Extracts county name from summary sheet row.
+        :param worksheet_row:
+        :return:
+        """
+        return worksheet_row[0]
 
-    :param worksheet_row:
-    :return:
-    """
-    return worksheet_row[1]
+    def _get_county(self, worksheet_row):
+        """
+        Extracts county name from summary sheet row.
 
+        :param worksheet_row:
+        :return:
+        """
+        return worksheet_row[1]
 
-def get_subcounty(worksheet_row):
-    """
-    Extracts district name from summary sheet row.
+    def _get_subcounty(self, worksheet_row):
+        """
+        Extracts district name from summary sheet row.
 
-    :param worksheet_row:
-    :return:
-    """
-    return worksheet_row[2]
+        :param worksheet_row:
+        :return:
+        """
+        return worksheet_row[2]
 
+    def _get_parrish(self, worksheet_row):
+        """
+        Extracts district name from summary sheet row.
 
-def get_parrish(worksheet_row):
-    """
-    Extracts district name from summary sheet row.
+        :param worksheet_row:
+        :return:
+        """
+        return worksheet_row[3]
 
-    :param worksheet_row:
-    :return:
-    """
-    return worksheet_row[3]
+    def _get_village(self, worksheet_row):
+        """
+        Extracts village name from summary sheet row.
 
+        :param worksheet_row:
+        :return:
+        """
+        return worksheet_row[4]
 
-def get_village(worksheet_row):
-    """
-    Extracts village name from summary sheet row.
+    def _get_trap_no(self, worksheet_row):
+        """
+        Extracts trap number from summary sheet row.
 
-    :param worksheet_row:
-    :return:
-    """
-    return worksheet_row[4]
+        :param worksheet_row:
+        :return:
+        """
+        return int(worksheet_row[5])
 
+    def _get_trap_latlong(self, worksheet_row):
+        """
+        Extracts trap's latitude and longitude from summary sheet row.
 
-def get_trap_no(worksheet_row):
-    """
-    Extracts trap number from summary sheet row.
+        :param worksheet_row:
+        :return:
+        """
+        lat = worksheet_row[6]
+        lon = worksheet_row[7]
 
-    :param worksheet_row:
-    :return:
-    """
-    return int(worksheet_row[5])
+        return "%s:%s" % (lat, lon)
 
+    def _get_trap_elevation(self, worksheet_row):
+        """
+        Extracts trap elevation from summary sheet row.
 
-def get_trap_latlong(worksheet_row):
-    """
-    Extracts trap's latitude and longitude from summary sheet row.
+        :param worksheet_row:
+        :return:
+        """
+        return float(worksheet_row[8])
 
-    :param worksheet_row:
-    :return:
-    """
-    lat = worksheet_row[6]
-    lon = worksheet_row[7]
+    def _get_trap_human_activity(self, worksheet_row):
+        """
+        Extracts human activity near trap from summary sheet row.
 
-    return "%s:%s" % (lat, lon)
+        :param worksheet_row:
+        :return:
+        """
+        return worksheet_row[9]
 
+    def _get_trap_vegetation(self, worksheet_row):
+        """
+        Extracts vegetation-type near trap from summary sheet row.
 
-def get_trap_elevation(worksheet_row):
-    """
-    Extracts trap elevation from summary sheet row.
+        :param worksheet_row:
+        :return:
+        """
+        return worksheet_row[10]
 
-    :param worksheet_row:
-    :return:
-    """
-    return float(worksheet_row[8])
+    def _get_trap_deployed_on(self, worksheet_row):
+        """
+        Extracts trap's deploy date from summary sheet row.
 
+        :param worksheet_row:
+        :return:
+        """
+        return cdata.convert_date_dd_mm_yy(worksheet_row[11])
 
-def get_trap_human_activity(worksheet_row):
-    """
-    Extracts human activity near trap from summary sheet row.
+    def _get_trap_removed_on(self, worksheet_row):
+        """
+        Extracts trap's removal date from summary sheet row.
 
-    :param worksheet_row:
-    :return:
-    """
-    return worksheet_row[9]
+        :param worksheet_row:
+        :return:
+        """
+        return cdata.convert_date_dd_mm_yy(worksheet_row[12])
 
+    def _get_other_info(self, worksheet_row):
+        """
+        Extracts "other info" column from summary sheet row.
 
-def get_trap_vegetation(worksheet_row):
-    """
-    Extracts vegetation-type near trap from summary sheet row.
-
-    :param worksheet_row:
-    :return:
-    """
-    return worksheet_row[10]
-
-
-def get_trap_deployed_on(worksheet_row):
-    """
-    Extracts trap's deploy date from summary sheet row.
-
-    :param worksheet_row:
-    :return:
-    """
-    return cdata.convert_date_dd_mm_yy(worksheet_row[11])
-
-
-def get_trap_removed_on(worksheet_row):
-    """
-    Extracts trap's removal date from summary sheet row.
-
-    :param worksheet_row:
-    :return:
-    """
-    return cdata.convert_date_dd_mm_yy(worksheet_row[12])
-
-
-def get_other_info(worksheet_row):
-    """
-    Extracts "other info" column from summary sheet row.
-
-    :param worksheet_row:
-    :return:
-    """
-    return worksheet_row[-1]
+        :param worksheet_row:
+        :return:
+        """
+        return worksheet_row[-1]
