3bf48d9349f1c5adddbc0f8d0d90230ebb08485a
Augustine Dunn <wadunn83@gmail.com>
2014-09-01 14:53:10 -0400

    tsetseDB/utils/parsing/data_import/summary_sheets.py: - finished converting to class scheme - still need to test more thouroughly

--- src/tsetseDB/utils/parsing/data_import/summary_sheets.py
+++ src/tsetseDB/utils/parsing/data_import/summary_sheets.py
@@ -12,39 +12,108 @@ Purpose:
 """
 __author__ = 'Gus Dunn'
 
-#from spartan.utils import spreadsheets as ss
+from bunch import Bunch
+
 from tsetseDB.utils.parsing import column_data as cdata
+from tsetseDB.utils.parsing.data_import import BaseImportSheet
+from tsetseDB.utils import errors
+from tsetseDB.utils.constants import get_village_id_map, get_village_ids
 
-# TODO: Convert below functions into a single class that logs file names etc for useful error reporting
 
-
-class SummarySheet(object):
-
-    def __init__(self, worksheet, workbook_path):
+class SummarySheet(BaseImportSheet):
+    """
+    Class to manage importing data from Robert's "survey summary" type excel sheets.
+    """
+    def __init__(self, engine, metadata, worksheet, workbook_path, season, trap_type='biconical', has_header_row=True):
         """
         Returns instance of `SummarySheet` constructed from `worksheet`.
+        :param engine:
+        :param metadata:
+        :param season:
+        :param trap_type:
+        :param has_header_row:
         :param worksheet: worksheet object
         :param workbook_path: location of source workbook file
         """
 
-        self.name = worksheet.name
-        self.worksheet = worksheet
-        self.path = workbook_path
+        # Calling base class __init__ for the rest.
+        BaseImportSheet.__init__(self, engine, metadata, worksheet, workbook_path, has_header_row=True)
 
-    def _build_trap_row(self, summary_sheet_row):
+        # Extending the base class __init__ for the rest
+        self.season = season
+        self.trap_type = trap_type
+        self.village_id_map = get_village_id_map()
+        self.village_ids = get_village_ids()
+        self._insert_on_table_functions = [self._insert_on_trap,
+                                           self._insert_on_village]
+
+    def _build_trap_row(self, worksheet_row):
         """
         Builds data that can be added to a `trap` table.
-        :param summary_sheet_row:
-        :return:
+        Returns a `dict`.
+        :param worksheet_row:
+        :return: `dict`
         """
-        raise NotImplementedError()
+        trap_row_dict = dict()
 
-    def _validate_summary_data_format(self):
+        trap_row_dict['trap_number'] = self._get_trap_no(worksheet_row=worksheet_row)
+        trap_row_dict['season'] = self.season
+        trap_row_dict['deploy_date'] = self._get_trap_deployed_on(worksheet_row=worksheet_row,
+                                                                  return_as='datetime.date')
+        trap_row_dict['removal_date'] = self._get_trap_removed_on(worksheet_row=worksheet_row,
+                                                                  return_as='datetime.date')
+        trap_row_dict['trap_type'] = self.trap_type
+        # get village NAME and convert to ID
+        trap_row_dict['village_id'] = self.village_id_map[self._get_village(worksheet_row=worksheet_row)]
+        trap_row_dict['gps_coords'] = self._get_trap_latlong(worksheet_row=worksheet_row)
+        trap_row_dict['elevation'] = self._get_trap_elevation(worksheet_row=worksheet_row)
+        #trap_row_dict['trap_id'] = "%s:%s" % (trap_row_dict["deploy_date"], trap_row_dict["gps_coords"])
+        trap_row_dict['other_info'] = self._get_other_info(worksheet_row=worksheet_row)
+
+        return trap_row_dict
+
+    def _build_village_row(self, worksheet_row):
         """
-        Returns `True` if `worksheet` passes format tests, `False` otherwise.
-        :return: `bool`
+        Builds data that can be added to a `village` table.
+        Returns a `dict`.
+        :param worksheet_row:
+        :return: `dict`
         """
-        raise NotImplementedError()
+        village_row_dict = dict()
+
+        # get village NAME and convert to ID
+        village_row_dict['village_id'] = self.village_id_map[self._get_village(worksheet_row=worksheet_row)]
+        village_row_dict["district"] = self._get_district(worksheet_row=worksheet_row)
+        village_row_dict["county"] = self._get_county(worksheet_row=worksheet_row)
+        village_row_dict["subcounty"] = self._get_subcounty(worksheet_row=worksheet_row)
+        village_row_dict["parish"] = self._get_parish(worksheet_row=worksheet_row)
+        village_row_dict["village_name"] = self._get_village(worksheet_row=worksheet_row)
+
+        return village_row_dict
+
+    def _insert_on_trap(self, connection):
+        results = Bunch()
+
+        # Generate INSERT calls populated with values from the current row for Trap table and submit to
+        trap_table = self.metadata.tables['trap']
+
+        trap_insert_values = self._build_trap_row(self.worksheet.row_values(self.current_row_number))
+        trap_insert_obj = trap_table.insert(values=trap_insert_values)
+        results.trap = connection.execute(trap_insert_obj)
+
+        return results
+
+    def _insert_on_village(self, connection):
+        results = Bunch()
+
+        # Generate INSERT calls populated with values from the current row for Village table and submit to
+        village_table = self.metadata.tables['village']
+
+        village_insert_values = self._build_village_row(self.worksheet.row_values(self.current_row_number))
+        village_insert_obj = village_table.insert(values=village_insert_values)
+        results.village = connection.execute(village_insert_obj)
+
+        return results
 
     def _get_district(self, worksheet_row):
         """
@@ -73,7 +142,7 @@ class SummarySheet(object):
         """
         return worksheet_row[2]
 
-    def _get_parrish(self, worksheet_row):
+    def _get_parish(self, worksheet_row):
         """
         Extracts district name from summary sheet row.
 
@@ -89,7 +158,23 @@ class SummarySheet(object):
         :param worksheet_row:
         :return:
         """
-        return worksheet_row[4]
+        village_string = worksheet_row[4]
+
+        if self._is_known_village(village_string):
+            pass
+        else:
+            msg = "%s, from data summary sheet: %s and row number: %s, is not currently a recognized village." % \
+                (village_string,
+                 self.worksheet.name,
+                 self.current_row_number + 1)
+
+            raise errors.TsetsedbImportError(msg)
+
+        if village_string in self.village_ids:
+            # we say we return the NAME not ID so lets make sure we do.
+            return self.village_id_map[village_string]
+        else:
+            return village_string
 
     def _get_trap_no(self, worksheet_row):
         """
@@ -139,23 +224,25 @@ class SummarySheet(object):
         """
         return worksheet_row[10]
 
-    def _get_trap_deployed_on(self, worksheet_row):
+    def _get_trap_deployed_on(self, worksheet_row, return_as='datetime.date'):
         """
         Extracts trap's deploy date from summary sheet row.
 
+        :param return_as:
         :param worksheet_row:
         :return:
         """
-        return cdata.convert_date_dd_mm_yy(worksheet_row[11])
+        return cdata.convert_date_dd_mm_yy(worksheet_row[11], return_as=return_as)
 
-    def _get_trap_removed_on(self, worksheet_row):
+    def _get_trap_removed_on(self, worksheet_row, return_as='datetime.date'):
         """
         Extracts trap's removal date from summary sheet row.
 
+        :param return_as:
         :param worksheet_row:
         :return:
         """
-        return cdata.convert_date_dd_mm_yy(worksheet_row[12])
+        return cdata.convert_date_dd_mm_yy(worksheet_row[12], return_as=return_as)
 
     def _get_other_info(self, worksheet_row):
         """
