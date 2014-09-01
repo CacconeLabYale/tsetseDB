afccb8b37d8d019f2dd22b6052addad9edfd4247
Gus Dunn <wadunn83@gmail.com>
2014-09-01 14:50:43 -0400

    tsetseDB/utils/errors.py: - added TsetsedbImportError

--- src/tsetseDB/utils/errors.py
+++ src/tsetseDB/utils/errors.py
@@ -14,7 +14,21 @@ Purpose:
 __author__ = 'Gus Dunn'
 
 
-class TsetseDBError(Exception):
+class TsetsedbError(Exception):
+    """
+    Base class for errors dealing with `tsetseDB` specific functionality.
+    """
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class TsetsedbImportError(TsetsedbError):
+    """
+    Error originating from errors in the format or contents of files provided as import material to `tsetseDB`.
+    """
     def __init__(self, value):
         self.value = value
 
